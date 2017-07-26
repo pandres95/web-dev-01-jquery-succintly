2405c232ca4250aec64248ffbdec1f4f36b41a08
Pablo Andrés Dorado Suárez <im@pandres95.me>
2017-07-25 22:04:29 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,47 @@
+(($, document) => {
+    'use strict';
+
+    // $ es la variable con la que generalmente nombramos a
+    // jQuery. Esto es por simplicidad, de modo que;
+    //
+    // en lugar de usar:
+    // jQuery('p.couldbered').addClass('red');
+    //
+    // usamos:
+    // $('p.couldbered').addClass('red');
+
+    // Primero se espera a que el documento cargue inicialmente
+
+    console.log($('a'));
+
+    $(document).ready(() => {
+        console.log('document#onload', $('a'));
+
+        $('a')
+            .text('Este es un link') // DOMElement.innerText
+            .attr('href', 'https://pandres95.me');
+
+        // Podemos crear elements usando jQuery
+        $('<a>')
+            .text('Example link')
+            .attr('href', 'https://example.com')
+            .appendTo('body');
+
+        let newParagraph = $('<p>')
+            .text('<p>Texto del parrafo</p>')
+            .appendTo('body');
+
+        let jQueryAnchor = $('<a>')
+            .appendTo('body');
+
+        // Asignar variables a los elementos/selectores de jQuery...
+        let coconutHeadImage = $('<img>')
+            .attr('src', 'https://vignette2.wikia.nocookie.net/ned/images/3/3b/Tumblr_lzxhgu5GA51rqtcv9o1_500.png/revision/latest?cb=20120909201041')
+            .attr('alt', 'Cabeza de coco');
+
+        // ... y retomar elementos mas tarde para manipulacion.
+        jQueryAnchor
+            .attr('href', 'https://jquery.com')
+            .html(coconutHeadImage);
+    });
+})(window.jQuery, window.document); // Pasamos a la funcion la libreria jQuery.
---
+++ js/tipos-funciones.js
@@ -0,0 +1,30 @@
+
+        // Lambda autoejecutable: expresión que devuelve un lambda y
+        // éste se ejecuta en el mismo instante
+
+        (() => {
+
+        })();
+
+        // Función autoejecutabl: expresión que devuelve una función
+        // y esta se ejecuta en el mismo instante
+
+        (function () {
+
+        })();
+
+        // Funciones con nombre
+
+        function nombre () {
+
+        }
+
+        nombre();
+
+        // Funciones como variables
+
+        var funcion = function () {
+
+        };
+
+        funcion();
